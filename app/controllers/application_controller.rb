8b9b67b96988910d0981aa52dc25d5d24e9c4411
Mark <web.markyn@gmail.com>
2019-11-13 19:21:44 +0300

    Finished project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
+  def logged_in
+    redirect_to users_path, notice: "Please log in" unless current_user
+  end
 end
+
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,34 +1,34 @@
 class EventsController < ApplicationController
+  before_action :logged_in, only: [:new, :create]
+  def new
+    @event = Event.new
+  end
 
-    def new
-        @event=Event.new
+  def create
+    @creator = User.find(session[:user_id])
+    p @creator
+    @event = @creator.created_events.build(event_params)
+    if @event.save
+      redirect_to users_path, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
     end
 
-    def create
-        @creator = User.find(session[:user_id])
-        p @creator
-        @event = @creator.created_events.build(event_params)
-          if @event.save
-            redirect_to users_path, notice: 'Event was successfully created.'
-          else
-            render :new
-          end
-      end
+  def index
+    @upcoming_events = Event.upcoming
+    @prev_events = Event.past
+  end
 
-      def index
-        @upcoming_events = Event.upcoming
-        @prev_events = Event.past
-      end
-
-      def show
-        @event=Event.find(params[:id])
-      end
+  def show
+    @event=Event.find(params[:id])
+  end
 
 
-      private 
+  private
 
-      def event_params
-        params.require(:event).permit(:date, :description)
+  def event_params
+    params.require(:event).permit(:date, :description)
 
-      end
+  end
 end
